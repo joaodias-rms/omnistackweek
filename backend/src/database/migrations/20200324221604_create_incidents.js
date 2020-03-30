e45094d4ba5e3dd1ea213092e7d28eebbf0f8c31
joão victor dias ramos <jvd.ramos@hotmail.com>
2020-03-29 23:12:43 -0300

    mobile da aplicação

--- backend/src/database/migrations/20200324221604_create_incidents.js
+++ backend/src/database/migrations/20200324221604_create_incidents.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
     return  knex.schema.createTable('incidents', function(table){
-        table.increments();
+        table.increments().primary();
         table.string('title').notNullable();
         table.string('description').notNullable();
         table.decimal('value').notNullable();
